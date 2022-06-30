8f0b4e463c88bd2bb4bb6667076ce8b34603f025
AbGrS <rahuldwivedi.rld@gmail.com>
2022-06-30 13:00:06 +0530

    Update pallindrome.js

--- pallindrome.js
+++ pallindrome.js
@@ -1,3 +1,4 @@
+// Check pallindrome
 var checkPallindrome = (str, start, end)=>{
 
  if(!start || !end){
@@ -17,3 +18,27 @@ var checkPallindrome = (str, start, end)=>{
 }
 
 console.log(checkPallindrome('abab'))
+
+
+// Reverse Words in String
+
+var reverseWordsInString = (str, start, end)=>{
+var arr = str.split(' ');
+  
+  if(!start || !end){
+  	 var start = 0, end = arr.length-1;
+  }
+ 
+  if(arr.length === 1 || start > end){
+    return arr.join(' ');
+  }
+  
+  var temp = arr[start];
+  arr[start] = arr[end];
+  arr[end] = temp;
+  
+ return reverseWordsInString(arr.join(' '), start+1, end-1);
+  
+}
+
+console.log(reverseWordsInString('I like very much'));
